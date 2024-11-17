807f9a0ea36b8fb4c755d85ed02863a811f1c478
jktjkt15 <jktjkt15@jktjkt15.com>
2024-11-17 17:43:22 -0500

    feat: test

--- lua/jayzone/lualine.lua
+++ lua/jayzone/lualine.lua
@@ -4,12 +4,12 @@ return {
 	inactive = {
 		a = { fg = colors.dark_grey, bg = colors.background },
 		b = { fg = colors.dark_grey, bg = colors.background },
-		c = { fg = colors.dark_grey, bg = colors.none },
+		c = { fg = colors.dark_grey, bg = colors.red },
 	},
 	normal = {
 		a = { fg = colors.white, bg = colors.blue },
 		b = { fg = colors.white, bg = colors.dark_grey },
-		c = { fg = colors.white, bg = colors.none },
+		c = { fg = colors.white, bg = colors.red },
 	},
 	visual = { a = { fg = colors.background, bg = colors.purple } },
 	replace = { a = { fg = colors.background, bg = colors.red } },
